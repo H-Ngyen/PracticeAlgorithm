16ab7169e891da1aedbc637d26b730d734055578
H-Ngyen <hoangnguyennn0210@gmail.com>
2025-08-16 02:58:07 +0700

    CSES: Number Spiral

---
+++ cses/Number_Spiral.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("unroll-loops,no-stack-protector")
+#define sz(x) int((x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sor(x) sort(all(x))
+#define rsor(x) sort(rall(x))
+#define vt vector
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define cel(x) cout << x << '\n'
+#define cht(x) cout << x << ' '
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve(){
+    int T; cin >> T; 
+    while (T--)
+    {
+        ll y, x, ans; cin >> y >> x;
+        if(y > x){
+            ans = (y - 1) * (y - 1);
+            if(y % 2 != 0) ans += x;
+            else ans += (2 * y - x); 
+        }
+        else{
+            ans = (x - 1) * (x - 1);
+            if(x % 2 == 0) ans += y;
+            else ans += (2 * x - y); 
+        }
+        cel(ans);
+    }
+}
+
+int main()
+{
+    fast_io;
+    solve();
+    
+    return 0;
+}
